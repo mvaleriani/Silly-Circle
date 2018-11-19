4bd01dbceb9a85d234862c592b13217bb5b5be1c
mvaleriani <michaelvaleriani@gmail.com>
2018-11-19 13:13:02 -0800

    first commit

---
+++ SillyCircle.jsx
@@ -0,0 +1,10 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+import App from './components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<App />, root);
+});
---
+++ components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <main id="App">
+        
+    </main>
+);
+
+export default App;
